66ad821fb86bf11f0e81bfb755465ec6cdf8086c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-10 19:50:08 +0200

    Created NavBar.js file with navigation NavBar component, splitting too big App component.

--- src/App.js
+++ src/App.js
@@ -63,22 +63,7 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
+      <NavBar query={query} onQuery={setQuery} movies={movies} />
 
       <main className="main">
         <div className="box">
@@ -164,5 +149,26 @@ export default function App() {
         </div>
       </main>
     </>
+  );
+}
+
+function NavBar({ query, onQuery, movies }) {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => onQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
   );
 }
