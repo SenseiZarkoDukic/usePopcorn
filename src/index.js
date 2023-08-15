1abb4c3aa2a8dd181ea87b744a9cd188945011a7
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-15 18:03:26 +0200

    Gave different maxRating in the index.js, put maxRating as a prop and it rendered stars as much the maxRating is. But when there is no maxRating defined we have 0 rendered stars so we added default value in the prop maxRating = 5, so by default when not defined it will be 5 stars.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
+import StarRating from "./StarRating";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </React.StrictMode>
 );
