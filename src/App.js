784a336a7d941d4eda71e9771681715efe59c08e
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-04 18:36:06 +0200

    Used Fetch in a wrong way, to better understand

--- src/App.js
+++ src/App.js
@@ -50,9 +50,14 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "c67106a4";
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -4,26 +4,22 @@ import { useState } from "react";
 // import './index.css';
 // import App from './App';
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
+import App from "./App";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <App />
   </React.StrictMode>
 );
