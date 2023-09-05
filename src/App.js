0da8ec931053b863c5972d6c8b55a9c51d01150e
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-05 17:11:33 +0200

    Created ErrorMessage component with message prop

--- src/App.js
+++ src/App.js
@@ -101,6 +101,16 @@ export default function App() {
 function Loader() {
   return <p className="loader">Loading...</p>;
 }
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔</span>
+      {message}
+    </p>
+  );
+}
+
 function NavBar({ children }) {
   return (
     <nav className="nav-bar">
