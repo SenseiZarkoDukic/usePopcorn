8e82123fa95ad0ee63464d3cf415ae9641d95205
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-11 18:53:40 +0200

    Add keydown event listener for Escape key

--- src/App.js
+++ src/App.js
@@ -99,6 +99,15 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
+  useEffect(function () {
+    document.addEventListener("keydown", function (e) {
+      if (e.code === "Escape") {
+        handleCloseMovie();
+        console.log("CLOSING");
+      }
+    });
+  }, []);
+
   useEffect(
     function () {
       const controller = new AbortController();
