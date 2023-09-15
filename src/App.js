4fbf18beac66ce938a4d665f670fad9096501f07
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-15 18:10:07 +0200

    Add watched movies to localStorage with useEffect hook, and add call back function to watched state to get watched movies from a localStorage

--- src/App.js
+++ src/App.js
@@ -56,10 +56,15 @@ const KEY = "c67106a4";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     // if (selectedId === null) setSelectedId(id);
@@ -81,6 +86,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
